0b2fc80f3e0f02b1c3a28539777138a132f75ae2
Sam <sbwmvb@umsystem.edu>
2020-09-20 16:20:15 -0500

    Providing links to projects and navigation bar

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -48,16 +48,33 @@ export class AboutComponent{
       desc: `I developed a website using Angular to display information important to employers
               in order to show experience in web development. Currently, I am working to develope 
               a backend for this site. `, 
-      link: "", 
+      link: "https://github.com/SamWeik/SamWeik.github.io", 
+      status: "Completed",
+      imgStat: "/assets/img/greenstat.png"
+    },
+    {
+      imgUrl: "/assets/img/bot.png", 
+      projectTitle: "InstagramBot", 
+      desc: `I developed an instagram bot to spam a selected target to learn how a bot operates and 
+              explore the power behind them.`, 
+      link: "https://github.com/SamWeik/InstaBotSpam", 
+      status: "Completed",
+      imgStat: "/assets/img/greenstat.png"
+    },
+    {
+      imgUrl: "/assets/img/CV.png", 
+      projectTitle: "CVproject", 
+      desc: `I developed a program using filters to detect motion, colors, and edges in a video.`, 
+      link: "https://github.com/SamWeik/CVproject", 
       status: "Completed",
       imgStat: "/assets/img/greenstat.png"
     },
     {
       imgUrl: "/assets/img/calendar.png", 
-      projectTitle: "Calendar", 
+      projectTitle: "Task Planner", 
       desc: `I am working to develop a calendar in python as a windows application to organize my 
               schedule for two weeks in order to improve time management and insure tasks are not forgotten. `, 
-      link: "", 
+      link: "https://github.com/SamWeik/TaskPlanner", 
       status: "In Progress",
       imgStat: "/assets/img/yellowstat.png"
     },
@@ -67,7 +84,7 @@ export class AboutComponent{
       desc: `A project with a small team of greek life students working to improve the way tasks are run
               at our fraternity. This project should allow fraternities to keep track of dailies, kitchen shifts,
               sober drives, events, and study hours.`, 
-      link: "", 
+      link: "https://github.com/SamWeik", 
       status: "Wait Listed",
       imgStat: "/assets/img/redstat.png"
     }
